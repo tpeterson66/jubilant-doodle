8a0bdd7dd3d17dc7da147a8adb1ab1d3d9df987d
Tom Peterson <tpeterson@Toms-Mac-mini.local>
2022-05-16 11:48:50 -0400

    save before lunch

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
   name     = "tpet66-rg-demo"
   location = "East US"
-  tags = {budget = "$100"}
+  tags     = { budget = "$100" }
 }
--- provider.tf
+++ provider.tf
@@ -1,10 +1,16 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.6.0"
     }
   }
+    backend "azurerm" {
+      resource_group_name  = "tpeterson080621"
+      storage_account_name = "tpeterson080621"
+      container_name       = "jubilant-doodle"
+      key                  = "terraform.tfstate"
+    }
 }
 
 provider "azurerm" {
